6a19b73277e821e7b6186f9b4608499e569076be
ZiyadMoustafa <ziyadmoustafa77@gmail.com>
2025-04-30 00:29:29 +0300

    add cookieParser

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const cors = require('cors');
+const cookieParser = require('cookie-parser');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -39,6 +40,8 @@ app.post(
 
 // Putting all data in the body into request obj to read it
 app.use(express.json());
+
+app.use(cookieParser());
 
 // Data sanitization against NoSQL query injection comments
 app.use(mongoSanitize());
